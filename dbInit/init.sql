41b54af7d5617dbb2cbef9f5a46d70f2576ba725
Thomas <theibergster@gmail.com>
2020-02-06 12:58:32 +0100

    added more tables to the init db file - added .htaccess, redirect to src folder

---
+++ dbInit/init.sql
@@ -0,0 +1,62 @@
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `type` enum('student','teacher','admin') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `videos` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `description` varchar(128),
+  `subject` varchar(64),
+  `rating` decimal(2,1),
+  `upload_time` timestamp NOT NULL,
+  `uploaded_by` bigint(20) NOT NULL,
+  `thumbnail` longblob NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`uploaded_by`) REFERENCES users(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `comments` (
+  `user_id` bigint(20) NOT NULL,
+  `video_id` bigint(20) NOT NULL,
+  `time` timestamp NOT NULL,
+  `comment` varchar(512) NOT NULL,
+  PRIMARY KEY (`user_id`,`video_id`,`time`),
+  FOREIGN KEY (`user_id`) REFERENCES users (id),
+  FOREIGN KEY (`video_id`) REFERENCES videos (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `playlists` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) NOT NULL,
+  `description` varchar(512),
+  `thumbnail` longblob,
+  `created_by` bigint(20) NOT NULL,
+  `time_created` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`created_by`) REFERENCES users (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `playlist_videos` (
+  `playlist_id` bigint(20) NOT NULL,
+  `video_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`playlist_id`,`video_id`),
+  FOREIGN KEY (`playlist_id`) REFERENCES playlists (id),
+  FOREIGN KEY (`video_id`) REFERENCES videos (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `subscriptions` (
+  `playlist_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`playlist_id`,`user_id`),
+  FOREIGN KEY (`playlist_id`) REFERENCES playlists (id),
+  FOREIGN KEY (`user_id`) REFERENCES users (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Create admin user --
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `type`) 
+VALUES (NULL, 'Admin User', 'admin@admin.no', '$2y$12$rciYrOAeONqVU7HjS6y32eRcLMJl16P/SRXJrTEm/Gu7litDRhyAa', 'admin');
